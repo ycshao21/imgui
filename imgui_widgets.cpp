391f2bf959cf973fe39f77fba91e18d17ea8935e
ocornut <omarcornut@gmail.com>
2023-05-24 15:16:17 +0200

    RangeSelect/MultiSelect: (Breaking) Rename ImGuiMultiSelectData to ImGuiMultiSelectIO.

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -6675,7 +6675,7 @@ bool ImGui::Selectable(const char* label, bool* p_selected, ImGuiSelectableFlags
 // The problem is that this may conflict with other behaviors of those items?
 //-------------------------------------------------------------------------
 
-static void DebugLogMultiSelectRequests(const ImGuiMultiSelectData* data)
+static void DebugLogMultiSelectRequests(const ImGuiMultiSelectIO* data)
 {
     ImGuiContext& g = *GImGui;
     if (data->RequestClear)     IMGUI_DEBUG_LOG_SELECTION("EndMultiSelect: RequestClear\n");
@@ -6683,7 +6683,7 @@ static void DebugLogMultiSelectRequests(const ImGuiMultiSelectData* data)
     if (data->RequestSetRange)  IMGUI_DEBUG_LOG_SELECTION("EndMultiSelect: RequestSetRange %p..%p = %d (dir %+d)\n", data->RangeSrc, data->RangeDst, data->RangeValue, data->RangeDirection);
 }
 
-ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void* range_ref, bool range_ref_is_selected)
+ImGuiMultiSelectIO* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void* range_ref, bool range_ref_is_selected)
 {
     ImGuiContext& g = *GImGui;
     ImGuiWindow* window = g.CurrentWindow;
@@ -6736,7 +6736,7 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     return &ms->In;
 }
 
-ImGuiMultiSelectData* ImGui::EndMultiSelect()
+ImGuiMultiSelectIO* ImGui::EndMultiSelect()
 {
     ImGuiContext& g = *GImGui;
     ImGuiMultiSelectTempData* ms = g.CurrentMultiSelect;
